6b4e7242f62a68976cacf55065ba03bb30dda0ee
BansiTilva <93094218+BansiTilva@users.noreply.github.com>
2023-05-24 11:35:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function division(){
+let val1= document.getElementById("a").value;
+let val2= document.getElementById("b").value;
+document.getElementById("result").value = val1/val2;
+}
+
+
+
+
+
+
+
+
+
